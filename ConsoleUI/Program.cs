c22cf92eacdd013f47931c47bc04a0e6b08ee648
zmitr-gen <zmitrovichevgeny@gmail.com>
2017-06-27 18:53:14 +0300

    Added logic QuickSort, ConsoleUI and XML comments. Sorry for troubles.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Logic.IntArrSorting;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void ShowArray(int[] arr)
+        {
+            foreach (int a in arr)
+            {
+                Console.Write(a + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array0 = { 2, 5, 7, 3, 6, 9, 5 };
+            int[] array1 = { 4, 1, 2, 3 };
+            int[] array2 = { };
+
+            int[] array = array0;
+
+            Console.WriteLine("Input array: ");
+            ShowArray(array);
+
+            QuickSort(array, 0, array.Length - 1);
+
+            Console.WriteLine("Output array: ");
+            ShowArray(array);
+        }
+    }
+}
---
+++ Logic/IntArrSorting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// The class helps to sort an array of type int[].
+    /// </summary>
+    public static class IntArrSorting
+    {
+        /// <summary>
+        /// Sorts int elements in an entire one-dimensional array.
+        /// </summary>
+        /// <param name="arr">
+        /// The one-dimensional int array to sort.
+        /// </param>
+        /// <param name="l">
+        /// Left bound index of the selected part of the array.
+        /// </param>
+        /// <param name="r">
+        /// Left bound index of the selected part of the array.
+        /// </param>
+        public static void QuickSort(int[] arr, int l, int r)
+        {
+            if (arr.Length == 0)
+                return;
+
+            int pivot = arr[l + (r - l) / 2];
+            int i = l;
+            int j = r;
+
+            while (i <= j)
+            {
+                for (; arr[i] < pivot; i++) ;
+                for (; arr[j] > pivot; j--) ;
+
+                if (i <= j)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (i < r)
+                QuickSort(arr, i, r);
+            if (j > l)
+                QuickSort(arr, l, j);
+        }
+
+    }
+}
